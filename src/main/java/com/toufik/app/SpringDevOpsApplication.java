da66ceac3bb7531ee7744f0bb9d3abda596b4434
taoufikSaadane <toufik.saadane@gmail.com>
2022-11-11 16:05:39 +0100

    initial

---
+++ src/main/java/com/toufik/app/SpringDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.toufik.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toufik/app/SpringDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toufik.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
